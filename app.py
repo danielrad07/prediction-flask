3f719d34d0975184331c8bc51c76fd08176b41b1
Daniel Rad <infographisteper@gmail.com>
2025-06-10 10:54:05 +0300

    Adaptation à Render : utilisation du port fourni par l'environnement

--- app.py
+++ app.py
@@ -45,4 +45,7 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
